2d9e4122054e29fa05e3cd66fa916cd4d814a3a9
Maksym Kryva <maksym.kryva@gmail.com>
2020-03-16 19:58:10 +0200

    Added peering

--- providers/aws/eu-west-1/prod/networking/vpc/peering.tf
+++ providers/aws/eu-west-1/prod/networking/vpc/peering.tf
@@ -1,11 +1,15 @@
-#module "prod_to_infra" {
-#  source  = "grem11n/vpc-peering/aws"
-#  version = "2.2.3"
-#  this_vpc_id          = module.vpc.vpc_id
-#  peer_vpc_id          = data.terraform_remote_state.infra_vpc.outputs.vpc_id
-#  auto_accept_peering  = true
-#
-#  tags = {
-#    Environment = "prod_infra"
-#  }
-#}
+module "prod_to_infra" {
+  source  = "grem11n/vpc-peering/aws"
+  version = "2.2.3"
+  this_vpc_id          = module.vpc.vpc_id
+  peer_vpc_id          = data.terraform_remote_state.infra_vpc.outputs.vpc_id
+  auto_accept_peering  = true
+  tags = {
+    Environment = "prod_infra"
+  }
+
+  providers = {
+    aws.this = aws
+    aws.peer = aws
+  }
+}
