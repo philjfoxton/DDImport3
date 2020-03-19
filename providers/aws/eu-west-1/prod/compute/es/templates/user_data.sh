edffc425aa1cd8f48da74e0475ee7c84f54462a8
Maksym Kryva <maksym.kryva@gmail.com>
2020-03-20 01:03:38 +0200

    add elk cluster

--- providers/aws/eu-west-1/prod/compute/es/templates/user_data.sh
+++ providers/aws/eu-west-1/prod/compute/es/templates/user_data.sh
@@ -2,7 +2,7 @@
 
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
-apt update && apt install awscli -y
+apt update && apt install awscli jq -y
 
 if [ "${bootstrap_node}" == "true"  ]; then
     while true
