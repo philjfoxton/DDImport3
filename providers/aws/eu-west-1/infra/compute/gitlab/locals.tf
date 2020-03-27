bc935a476c6406fb51f1350cfa91c892e187d635
Maksym Kryva <maksym.kryva@gmail.com>
2020-03-27 12:19:03 +0200

    fixes

--- providers/aws/eu-west-1/infra/compute/gitlab/locals.tf
+++ providers/aws/eu-west-1/infra/compute/gitlab/locals.tf
@@ -1,5 +1,5 @@
 locals {
   gitlab_runner_version = "12.9.0" # https://gitlab.com/gitlab-org/gitlab-runner/tags
-  runner_name = "gitlab-bastion-runner"
-  environment = "production"
+  runner_name           = "gitlab-bastion-runner"
+  environment           = "production"
 }
