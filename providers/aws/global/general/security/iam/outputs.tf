0e886aeb64a56824a949e285130f236ff58c8802
Maksym Kryva <maksym.kryva@gmail.com>
2020-03-30 11:33:25 +0300

    output

--- providers/aws/global/general/security/iam/outputs.tf
+++ providers/aws/global/general/security/iam/outputs.tf
@@ -1,3 +1,31 @@
 output "external_dns_role" {
   value = module.external_dns_role.this_iam_role_arn
 }
+
+output "connections_service_role" {
+  value = module.connections_service.this_iam_role_arn
+}
+
+output "goals_service_role" {
+  value = module.goals_service.this_iam_role_arn
+}
+
+output "notifications_api_role" {
+  value = module.notifications_api.this_iam_role_arn
+}
+
+output "notifications_service_role" {
+  value = module.notifications_service.this_iam_role_arn
+}
+
+output "obpexporter_service_role" {
+  value = module.obpexporter_service.this_iam_role_arn
+}
+
+output "transactions_service_role" {
+  value = module.transactions_service.this_iam_role_arn
+}
+
+output "statements_service_role" {
+  value = module.statements_service.this_iam_role_arn
+}
