7bbb5db0ff010eaaf9fa232f4fcacf3356e77b30
Maksym Kryva <maksym.kryva@gmail.com>
2020-04-01 09:12:28 +0300

    cleanup

--- providers/kubernetes/prod/helm_vector.tf
+++ providers/kubernetes/prod/helm_vector.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "vector" {
   name      = "vector"
   chart     = "${path.module}/charts/vector"
-  namespace = kubernetes_namespace.observability.metadata.0.name
+  namespace = kubernetes_namespace.prod.metadata.0.name
   values = [
     file("${path.module}/values/vector.yaml")
   ]
